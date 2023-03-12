ead9faebcc3a6333165c2ba5cf2adfda6d9b64e8
alissonpceva <axc2980@miami.edu>
2023-03-12 18:52:29 -0400

    final changes to JS

--- Develop/script.js
+++ Develop/script.js
@@ -11,62 +11,59 @@ var selectedCharacters = [];
 
 // select password length 
 
-passwordLength = Number(window.prompt('Choose between 8-128 characters for your password.'));
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert ('Please choose a number between 8 to 128.');
-  } else (window.alert('Your password will be ' + passwordLength + ' characters long'));
- 
-    // click ok to confirm special characters
-    confSpecialChar = confirm("Confirm Special Characters for your password")
-    if (confSpecialChar == true) {
-      window.alert('Your password will have special characters');
-      selectedCharacters = selectedCharacters.concat(specialCharacter);
-    } else {
-      window.alert('Your password will NOT have special characters');
-    }
-    // click ok to confirm including numeric characters
-    confNumChar = confirm("Confirm Numeric Characters for your password")
-    if (confNumChar == true) {
-      window.alert('Your password will have numeric characters');
-      selectedCharacters = selectedCharacters.concat(numericCharacters);
-    } else {
-      window.alert('Your password will NOT have numeric characters');
-    }
-    // click ok to confirm including lower characters
-      confLowCase = confirm("Confirm lower case letters for your password")
-      if (confLowCase == true) {
-        window.alert('Your password will have lower case letters');
-        specialCharacter = specialCharacter.concat(lowerCase);
-      } else {
-        window.alert('Your password will NOT have lower case letters');
-      }
-    // click ok to confirm including upper characters 
-      confUpperCase = confirm("Confirm UPPER CASE LETTERS for your password")
-      if (confUpperCase == true) {
-        window.alert('Your password will have UPPER CASE LETTERS');
-        specialCharacter = specialCharacter.concat(upperCase)
-      } else {
-        window.alert('Your password will NOT have UPPER CASE LETTERS');
-      }
-    
-      // if no conditions are selected, alert will pop up and generatepassword will re run
-    if (confSpecialChar === !true && confNumChar === !true && confLowCase === !true && confUpperCase === !true) {
-      alert('Please select at least one condition');}
-      generatePassword();
-      return;
-    
-// selectedCharacters[Math.floor(Math.random() * passwordLength.length)];
-//   for (let i = 0; i < passwordLength; i++) {
-//     }
-  }
-
-// // function to create password based on conditions
-
-function finalPassword() {
-  return selectedCharacters[Math.floor(Math.random() * passwordLength.length)];
+var passwordLength = Number(window.prompt('Choose between 8-128 characters for your password.'));
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert ('Please choose a number between 8 to 128.');
+  return; // Exit the function if password length is invalid
+} else {
+  window.alert('Your password will be ' + passwordLength + ' characters long');
 }
 
-// function createPassword  
+var confSpecialChar = confirm("Confirm Special Characters for your password")
+if (confSpecialChar == true) {
+  window.alert('Your password will have special characters');
+  selectedCharacters = selectedCharacters.concat(specialCharacter);
+} else {
+  window.alert('Your password will NOT have special characters');
+}
+
+var confNumChar = confirm("Confirm Numeric Characters for your password")
+if (confNumChar == true) {
+  window.alert('Your password will have numeric characters');
+  selectedCharacters = selectedCharacters.concat(numericCharacters);
+} else {
+  window.alert('Your password will NOT have numeric characters');
+}
+
+var confLowCase = confirm("Confirm lower case letters for your password")
+if (confLowCase == true) {
+  window.alert('Your password will have lower case letters');
+  selectedCharacters = selectedCharacters.concat(lowerCase);
+} else {
+  window.alert('Your password will NOT have lower case letters');
+}
+
+var confUpperCase = confirm("Confirm UPPER CASE LETTERS for your password")
+if (confUpperCase == true) {
+  window.alert('Your password will have UPPER CASE LETTERS');
+  selectedCharacters = selectedCharacters.concat(upperCase);
+} else {
+  window.alert('Your password will NOT have UPPER CASE LETTERS');
+}
+
+if (confSpecialChar !== true && confNumChar !== true && confLowCase !== true && confUpperCase !== true) {
+  alert('Please select at least one condition');
+  return; // Exit the function if no character type is selected
+}
+
+var password = '';
+for (var i = 0; i < passwordLength; i++) {
+  var randomIndex = Math.floor(Math.random() * selectedCharacters.length);
+  password += selectedCharacters[randomIndex];
+}
+
+return password;
+}
   // Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
   
